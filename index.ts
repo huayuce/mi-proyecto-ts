28c8a51ef1cf9ab58df7706f542976f20365728a
huayuce <edgaruceda@gmail.com>
2026-09-12 02:10:21 -0500

    Feat: Creaación de la primera clase Persona en typeScript

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
 console.log("¡Hola Mundo desde TypeScript!");
+
+//==========================================
+//EJEMPLO : CLASES AL ESTILO JAVA
+//==========================================
+
+class Persona {
+    //1. Atributos con modificadores de acceso (igual que en java)
+    private nombre: string;
+    public edad: number;
+
+    //2. El constructor (en TS siempre se usa la palbra clave 'constructor')
+    constructor(nombre: string, edad: number){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    //3. Un método comun y corriente
+    public presentar(): string {
+        return `Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años.`;
+    }
+}
+
+const programador = new Persona("Edgar", 51);
+console.log(programador.presentar());
